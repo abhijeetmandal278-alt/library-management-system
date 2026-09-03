900d5e7449afb47a3926c7935fdcf749b2ba37c9
Abhijeet6080 <am5185@srmist.edu.in>
2026-09-04 03:46:46 +0530

    "Update API URL and remove proxy for production"

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const apiInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
-  headers: {
+baseURL: import.meta.env.VITE_API_URL || 'https://library-management-system-hfuo.onrender.com/api',  headers: {
     'Content-Type': 'application/json'
   }
 });
